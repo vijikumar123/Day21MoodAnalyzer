491e27c21de9daf5e09063db19be0ba9dce9e843
vijikumar123 <vk9164940756@gmail.com>
2023-02-13 00:27:35 +0530

    Added:uc1AnalyzeMood

--- src/main/java/com/bridgelabz/MoodAnalysisException.java
+++ src/main/java/com/bridgelabz/MoodAnalysisException.java
@@ -1,14 +1,6 @@
 package com.bridgelabz;
 
 public class MoodAnalysisException  extends Exception{
-    public MoodAnalysisException(String message, Exception_Type type) {
-        super(message);
-
-    }
-
-    public enum Exception_Type {
-        NULL, EMPTY;
-    }
 
 }
 
--- src/main/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/main/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,11 +1,6 @@
 package com.bridgelabz;
 
-import Com.bridgeLabs.MoodAnalysisException;
 import Com.bridgeLabs.MoodAnalyzerMain;
-import org.junit.Assert;
-import org.junit.Test;
-
-import static org.junit.Assert.assertSame;
 import org.junit.Test;
 
 import static org.junit.Assert.assertSame;
@@ -18,21 +13,9 @@ public class MoodAnalyzerTest {
         MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("I am In Sad Mood");
         String mood = moodAnalyzer.analyseMood();
         assertSame("SAD", mood);
-    }
-    //Constructor in message should return Happy
 
-    @Test
-    public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am in Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        assertSame("HAPPY", mood);
     }
 
-    @Test
-    public void given_NULLMood_Should_Return_HAPPY() {
-        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain(null);
-        String mood = moodAnalyser.analyseMood();
-        assertSame("HAPPY", mood);
-    }
 }
+
 
