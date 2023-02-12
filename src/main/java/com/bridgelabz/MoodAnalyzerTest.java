02e67195c467d3da6a98f60853cd4291ea8af1a2
vijikumar123 <vk9164940756@gmail.com>
2023-02-13 00:35:20 +0530

    Added:uc2ExceptionHandling

--- src/main/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/main/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -4,6 +4,9 @@ import Com.bridgeLabs.MoodAnalyzerMain;
 import org.junit.Test;
 
 import static org.junit.Assert.assertSame;
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
 
 
 public class MoodAnalyzerTest {
@@ -13,8 +16,16 @@ public class MoodAnalyzerTest {
         MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("I am In Sad Mood");
         String mood = moodAnalyzer.analyseMood();
         assertSame("SAD", mood);
-
     }
+    //Constructor in message should return Happy
+
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am in Happy Mood");
+        String mood = moodAnalyser.analyseMood();
+        assertSame("HAPPY", mood);
+    }
+}
 
 }
 
