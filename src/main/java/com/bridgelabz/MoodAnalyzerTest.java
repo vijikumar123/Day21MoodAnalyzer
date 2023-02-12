c2d8688cee98ee739bd76a2dd37a72ee5befc0ce
vijikumar123 <vk9164940756@gmail.com>
2023-02-13 00:13:44 +0530

    Added:TC-1.2  MoodAnalyzer

--- src/main/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/main/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -12,21 +12,10 @@ import static org.junit.Assert.assertSame;
 
 
 public class MoodAnalyzerTest {
-    //Test case for Sad mood
     @Test
     public void given_SadMood_Should_Return_SAD() {
         MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain();
         String mood = moodAnalyzer.analyseMood("I am In A Sad Mood");
         Assert.assertEquals("SAD", mood);
-
-    }
-    // Test case for Happy mood
-
-    @Test
-    public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain();
-        String mood = moodAnalyser.analyseMood("I am in Any Mood");
-        Assert.assertEquals("HAPPY", mood);
     }
 }
-
